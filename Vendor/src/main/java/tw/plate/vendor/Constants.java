75bc7bbc35d8738c5f49cdd0e0f44fa5881e0ec1
Anthony Ang <anthonyang86@gmail.com>
2014-02-03 12:56:48 +0800

    switched to httpswq

--- Vendor/src/main/java/tw/plate/vendor/Constants.java
+++ Vendor/src/main/java/tw/plate/vendor/Constants.java
@@ -11,7 +11,7 @@ final public class Constants {
     //public static final String API_URI_PREFIX = "http://192.168.0.192:8080"; // Heron's Local Developing Mode
     //public static final String API_URI_PREFIX = "http://106.187.89.91:8080"; // Heron's Local Developing Mode
     //public static final String API_URI_PREFIX = "http://api-dev.plate.tw:8080"; // Heron's Local Developing Mode
-    public static final String API_URI_PREFIX = "http://api.plate.tw"; // Release Mode
+    public static final String API_URI_PREFIX = "https://api.plate.tw"; // Release Mode
 
     //
     public static final String LOG_TAG = "PlateVendorLog";

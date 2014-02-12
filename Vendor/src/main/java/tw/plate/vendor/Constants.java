9db3461caf9c7309a925ad82b19e1e78b3b11ea7
Heron Yang <v6510young88@gmail.com>
2014-02-12 11:31:51 +0800

    add custom logo

--- Vendor/src/main/java/tw/plate/vendor/Constants.java
+++ Vendor/src/main/java/tw/plate/vendor/Constants.java
@@ -6,7 +6,7 @@ package tw.plate.vendor;
 final public class Constants {
     // SERVER URI
     //public static final String API_URI_PREFIX = "http://10.0.2.2:8080"; // Developing Mode (Emulator)
-    //public static final String API_URI_PREFIX = "http://192.168.0.115:8080"; // Heron's Local Developing Mode
+    //public static final String API_URI_PREFIX = "http://192.168.0.121:8080"; // Heron's Local Developing Mode
     //public static final String API_URI_PREFIX = "http://192.168.1.109:8080"; // Heron's Local Developing Mode
     //public static final String API_URI_PREFIX = "http://192.168.0.192:8080"; // Heron's Local Developing Mode
     //public static final String API_URI_PREFIX = "http://106.187.89.91:8080"; // Heron's Local Developing Mode
